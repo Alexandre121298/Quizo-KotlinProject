8db2cd055a5f0ad2623f1a0e85b4149d25c78478
Astre80 <alex.dub8012@outlook.fr>
2023-10-20 16:08:48 +0200

    Modification de la TopAppBar

--- app/src/main/java/fr/upjv/quizo/ui/MainActivity.kt
+++ app/src/main/java/fr/upjv/quizo/ui/MainActivity.kt
@@ -3,27 +3,67 @@ package fr.upjv.quizo.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import fr.upjv.quizo.R
 import fr.upjv.quizo.ui.theme.QuizoTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
+    val DarkGrey = fr.upjv.quizo.ui.theme.DarkGrey
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             QuizoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold(
+                    topBar = {
+                        TopAppBar( //Barre de navigation en haut de l'ecran
+                            navigationIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.ArrowBack,
+                                    contentDescription = null,
+                                    tint = Color.White
+                                )
+                            },
+                            title = {
+                                Text(text = stringResource(id = R.string.app_name),
+                                    color = Color.White)
+                                    },
+                            colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = DarkGrey)
+                        )
+                    },
+                    bottomBar = {
+                        BottomAppBar { Text("Work in progress") }
+                    },
+                )
+                { paddingValues ->
+                    Text(
+                        modifier = Modifier.padding(paddingValues),
+                        text = "content"
+                    )
                 }
+
             }
         }
     }
